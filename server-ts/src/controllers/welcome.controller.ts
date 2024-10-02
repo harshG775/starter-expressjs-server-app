33c111e19bf8d4bba820a60f00a52727ed7e1b86
Harsh Gaur <hgaur491@gmail.com>
2024-10-02 13:19:01 +0530

    ///

--- server-ts/src/controllers/welcome.controller.ts
+++ server-ts/src/controllers/welcome.controller.ts
@@ -1,8 +1,8 @@
-import { Response } from "express";
+import { Request, Response } from "express";
 import { asyncHandler } from "@/utils/asyncHandler";
 import responseStatus from "@/utils/responseStatus";
 
-export const welcome = asyncHandler(async (res: Response) => {
+export const welcome = asyncHandler(async (req: Request, res: Response) => {
     responseStatus.ok(res, {
         message: "welcome to version 1.0.0 of the api",
         version: "1.0.0",
