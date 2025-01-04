43a4fc686c17dac8a90e3b5458d2c82987d476a4
Harsh Gaur <hgaur491@gmail.com>
2025-01-04 21:02:22 +0530

    adding eslint

--- server-ts-boilerplate/src/middlewares/notFound.middleware.ts
+++ server-ts-boilerplate/src/middlewares/notFound.middleware.ts
@@ -2,8 +2,7 @@ import { ResponseError } from "../exception";
 import { Request, Response } from "express";
 import { StatusCodes, ReasonPhrases } from "http-status-codes";
 
-export const notFoundMiddleware = (req: Request, res: Response) => {
-    // res.status(StatusCodes.NOT_FOUND).json({ message: ReasonPhrases.NOT_FOUND, status: StatusCodes.NOT_FOUND });
+export const notFoundMiddleware = (req: Request, _res: Response) => {
     throw new ResponseError({
         statusCode: StatusCodes.NOT_FOUND,
         message: ReasonPhrases.NOT_FOUND,
