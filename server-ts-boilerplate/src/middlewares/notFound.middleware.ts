c40be372a632a7db25e541f8908113d6c5e2cad2
Harsh Gaur <hgaur491@gmail.com>
2025-01-04 20:10:37 +0530

    adding ResponseError in notFound

--- server-ts-boilerplate/src/middlewares/notFound.middleware.ts
+++ server-ts-boilerplate/src/middlewares/notFound.middleware.ts
@@ -1,6 +1,12 @@
+import { ResponseError } from "../exception";
 import { Request, Response } from "express";
 import { StatusCodes, ReasonPhrases } from "http-status-codes";
 
-export const notFoundMiddleware = (_req: Request, res: Response) => {
-    res.status(StatusCodes.NOT_FOUND).json({ message: ReasonPhrases.NOT_FOUND, status: StatusCodes.NOT_FOUND });
+export const notFoundMiddleware = (req: Request, res: Response) => {
+    // res.status(StatusCodes.NOT_FOUND).json({ message: ReasonPhrases.NOT_FOUND, status: StatusCodes.NOT_FOUND });
+    throw new ResponseError({
+        statusCode: StatusCodes.NOT_FOUND,
+        message: ReasonPhrases.NOT_FOUND,
+        context: { route: req.originalUrl },
+    });
 };
