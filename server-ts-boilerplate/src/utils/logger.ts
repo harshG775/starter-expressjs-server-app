59188c5a698274a087588ede8a0a697b7804ac6a
Harsh Gaur <hgaur491@gmail.com>
2025-01-12 17:07:30 +0530

    using join to join tmp folder

--- server-ts-boilerplate/src/utils/logger.ts
+++ server-ts-boilerplate/src/utils/logger.ts
@@ -1,5 +1,6 @@
 import { config } from "../constants";
 import winston from "winston";
+import { join } from "path";
 
 const isDevelopment = config.server.nodeEnv === "development";
 const defaultMeta = { service: "express-server" };
@@ -36,8 +37,8 @@ const format = winston.format.combine(
 );
 const transports = [
     new winston.transports.Console(),
-    new winston.transports.File({ filename: "tmp/error.log", level: "error" }),
-    new winston.transports.File({ filename: "tmp/all.log" }),
+    new winston.transports.File({ filename: join("/tmp", "logs/error.log"), level: "error" }),
+    new winston.transports.File({ filename: join("/tmp", "logs/all.log") }),
 ];
 export const logger = winston.createLogger({
     defaultMeta,
