d30e8ad13f4483191ae09de70b11f227fd9784da
Harsh Gaur <hgaur491@gmail.com>
2025-01-25 14:06:33 +0530

    removing unnecessary code

--- server-ts-boilerplate/src/routes/health.routes.ts
+++ server-ts-boilerplate/src/routes/health.routes.ts
@@ -1,5 +1,6 @@
 import { healthController } from "@/controllers";
+import { asyncHandler } from "@/utils";
 import { Router } from "express";
 export const health = (router: Router): void => {
-    router.route("/").get(healthController.health);
+    router.route("/").get(asyncHandler(healthController.health));
 };
--- server-ts-boilerplate/src/utils/asyncHandler.ts
+++ server-ts-boilerplate/src/utils/asyncHandler.ts
@@ -1,22 +1,7 @@
-import { ResponseError } from "@/exception";
 import { Request, Response, NextFunction } from "express";
-import { ReasonPhrases, StatusCodes } from "http-status-codes";
-
 type AsyncFunction = (req: Request, res: Response, next: NextFunction) => Promise<void>;
-
 export const asyncHandler = (fn: AsyncFunction) => {
     return (req: Request, res: Response, next: NextFunction) => {
-        fn(req, res, next).catch((err) => {
-            if (err instanceof ResponseError) {
-                // Handle custom errors
-                return res.status(err.statusCode).json(err.errorDetails);
-            }
-
-            // Handle unexpected errors
-            console.error("Unexpected Error:", err);
-            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-                errors: [{ message: ReasonPhrases.INTERNAL_SERVER_ERROR}],
-            });
-        });
+        fn(req, res, next).catch(next); // Automatically forwards errors to the global error handler
     };
 };
