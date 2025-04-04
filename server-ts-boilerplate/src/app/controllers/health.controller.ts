2a249f7192d8b54e1aa6e907a95f001fde001691
Harsh Gaur <hgaur491@gmail.com>
2025-04-04 23:29:19 +0530

    //

--- server-ts-boilerplate/src/app/controllers/health.controller.ts
+++ server-ts-boilerplate/src/app/controllers/health.controller.ts
@@ -4,7 +4,6 @@ import { Request, Response } from "express";
 
 export const healthController = {
     health: catchAsync(async (_req: Request, res: Response) => {
-        res.status(200).json({ message: "Server is running" });
+        res.status(200).json({ success: true, message: "Server is running" });
     }),
 };
-
