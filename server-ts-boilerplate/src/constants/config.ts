7ff9a459080ac79fb97a0d3a32263df4f4dc6136
Harsh Gaur <hgaur491@gmail.com>
2025-04-06 22:25:23 +0530

    //

--- server-ts-boilerplate/src/constants/config.ts
+++ server-ts-boilerplate/src/constants/config.ts
@@ -1,7 +1,7 @@
 export const config = {
     server: {
-        version: process.env.APP_VERSION || "v1",
-        port: parseInt(process.env.APP_PORT || "8000"),
+        version: process.env.VERSION || "v1",
+        port: parseInt(process.env.PORT || "8000"),
         allowedOrigins: JSON?.parse(process.env.ALLOWED_ORIGINS || "[]") as string[],
         nodeEnv: process?.env?.NODE_ENV || "development",
     },
