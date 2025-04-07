4083d77b7724483475878e2472a076c17798196b
Harsh Gaur <hgaur491@gmail.com>
2025-04-07 16:12:14 +0530

    test

--- server-ts-boilerplate/dist/index.js
+++ server-ts-boilerplate/dist/index.js
@@ -273,6 +273,8 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(morganMiddleware);
 app.use("/public", express.static(path.join(cwd(), "/public")));
+console.log(`path.join(cwd(), "/public")`);
+console.log(path.join(cwd(), "/public"));
 app.use("/api/docs/reference", configureOpenAPI);
 app.use("/api", router);
 app.use(notFoundMiddleware);
