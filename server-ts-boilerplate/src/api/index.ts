9ea87881e2a6be2317711a2db872d5251f461fcc
Harsh Gaur <hgaur491@gmail.com>
2025-04-08 21:09:13 +0530

    ///

--- server-ts-boilerplate/src/api/index.ts
+++ server-ts-boilerplate/src/api/index.ts
@@ -2,9 +2,11 @@ import express from "express";
 import { router } from "./router/index.js";
 import { errorHandler } from "@/exception/index.js";
 import { notFoundMiddleware } from "@/middlewares/index.js";
+import { config } from "@/config/index.js";
 const app = express();
+const { version } = config;
 
-app.use("/api", router);
+app.use(`/api/v${version}`, router);
 app.use(notFoundMiddleware);
 app.use(errorHandler);
 
