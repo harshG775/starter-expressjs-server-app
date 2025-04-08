6d2040de8de33c7e736bb6cda9e3429dc5c3987c
Harsh Gaur <hgaur491@gmail.com>
2025-04-08 22:10:28 +0530

    rate limit

--- server-ts-boilerplate/src/config/index.ts
+++ server-ts-boilerplate/src/config/index.ts
@@ -7,8 +7,12 @@ const config = {
     host: process.env.HOST || "localhost",
     version: process.env.VERSION || "1.0.0",
     allowedCorsOrigins: JSON.parse(process.env.ALLOWED_CORS_ORIGINS || '["http://localhost:8000"]'),
+
+    rateLimit: {
+        windowMs: (Number(process.env.RATE_LIMIT_WINDOW_MS) || 5) * 60 * 1000, // Time window (default 5 minutes)
+        maxRequest: Number(process.env.RATE_LIMIT_MAX_REQUESTS) || 100,
+        message: process.env.RATE_LIMIT_MESSAGE || "Too many requests, please try again later.",
+    },
 };
 export { config };
 console.log(config);
-
-
