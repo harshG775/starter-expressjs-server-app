742a5e0237e60f656748a1f43603724970387fc3
Harsh Gaur <hgaur491@gmail.com>
2025-04-08 20:45:45 +0530

    health

--- server-ts-boilerplate/src/api/controllers/health.controller.ts
+++ server-ts-boilerplate/src/api/controllers/health.controller.ts
@@ -1,7 +1,18 @@
 import { NextFunction, Request, Response } from "express";
 import { StatusCodes, ReasonPhrases } from "http-status-codes";
 
-const health = async (_req: Request, res: Response, _next: NextFunction) => {
-    res.status(StatusCodes.OK).json({ message: ReasonPhrases.OK, status: StatusCodes.OK });
+const health = async (_req: Request, res: Response, next: NextFunction) => {
+    try {
+        res.status(StatusCodes.OK).json({
+            success: true,
+            message: ReasonPhrases.OK,
+            status: StatusCodes.OK,
+            uptime: process.uptime(),
+            timestamp: new Date().toISOString(),
+        });
+    } catch (error) {
+        next(error);
+    }
 };
+
 export const healthController = { health };
