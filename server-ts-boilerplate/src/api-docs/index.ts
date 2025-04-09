682a1c45204fee5ce79179a090f95854e0fdfe2c
Harsh Gaur <hgaur491@gmail.com>
2025-04-09 18:48:27 +0530

    ///

--- server-ts-boilerplate/src/api-docs/index.ts
+++ server-ts-boilerplate/src/api-docs/index.ts
@@ -3,21 +3,18 @@ import { OpenAPIRegistry, OpenApiGeneratorV3 } from "@asteasolutions/zod-to-open
 
 export const registry = new OpenAPIRegistry();
 
-export const openApiConfig = {
-    openapi: "3.0.3",
-    info: {
-        title: "Your API",
-        version: "1.0.0",
-    },
-    // servers: [{ url: "http://localhost:3000" }],
-    externalDocs: {
-        description: "View the raw OpenAPI Specification in JSON format",
-        url: "/openapi.json",
-    },
-};
-
-
 export function generateOpenApiSpec() {
     const generator = new OpenApiGeneratorV3(registry.definitions);
-    return generator.generateDocument(openApiConfig);
+    return generator.generateDocument({
+        openapi: " 3.1.0",
+        info: {
+            title: "Your API",
+            version: "1.0.0",
+        },
+        // servers: [{ url: "http://localhost:3000" }],
+        externalDocs: {
+            description: "View the raw OpenAPI Specification in JSON format",
+            url: "/openapi.json",
+        },
+    });
 }
