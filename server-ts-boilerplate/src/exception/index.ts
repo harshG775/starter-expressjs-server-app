af41a2ca46e67b31cb7bda48a3148867ba57d16e
Harsh Gaur <hgaur491@gmail.com>
2025-04-11 13:50:30 +0530

    ////////////////////////////////////////

--- server-ts-boilerplate/src/exception/index.ts
+++
@@ -1,3 +0,0 @@
-export { HttpException } from "./httpException.js";
-export { NotFoundException } from "./notFoundException.js";
-export { errorHandler } from "./errorHandler.js";
--- server-ts-boilerplate/src/middlewares/notFound.middleware.ts
+++
@@ -1,6 +0,0 @@
-import { NotFoundException } from "@/exception/notFoundException.js";
-import { Request, Response, NextFunction } from "express";
-
-export const notFoundMiddleware = (req: Request, _res: Response, next: NextFunction) => {
-    next(new NotFoundException("Route not found", [{ method: req.method, path: req.path }]));
-};
