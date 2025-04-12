e849ef396a7586d0f8a752dbd0f9eb3a628cbae3
Harsh Gaur <hgaur491@gmail.com>
2025-04-12 11:19:22 +0530

    build

--- server-ts-boilerplate/dist/server.js
+++
@@ -1,6 +0,0 @@
-import { app } from "./app/index.js";
-import { config } from "./config/index.js";
-const { port, host } = config;
-app.listen(port, async () => {
-    console.info(`Server is running on port http://${host}:${port}`);
-});
